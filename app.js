c0a5a0cec750d857856df77a161d91ef8244fe0a
sreejeshd <sreejesh.divakaran@gmail.com>
2025-02-23 13:42:47 -0500

    Commiting the updated app.js document to trigger the jenkins pipeline

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var broken = false;
+// Import required modules
+const express = require('express');
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const broken = false;
 
-var indexRouter = require('./routes/index');
-var trainsRouter = require('./routes/trains');
+const indexRouter = require('./routes/index');
+const trainsRouter = require('./routes/trains');
 
-var app = express();
+const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -21,44 +22,45 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-   res.locals = {
-     broken: broken
-   };
-   next();
+  res.locals = { broken: broken };
+  next();
 });
 
 app.use('/', indexRouter);
 app.use('/trains', trainsRouter);
 
-//this endpoint performs cpu-intensive calculations
+// This endpoint performs CPU-intensive calculations
 app.get('/generate-cpu-load', function(req, res, next) {
-  var val = 0.0001
-  for (i = 0; i < 1000000; i++) {
+  let val = 0.0001;
+  for (let i = 0; i < 1000000; i++) {
     val += Math.sqrt(val);
   }
-  res.status(200).send('Doing a bunch of calculations!')
+  res.status(200).send('Doing a bunch of calculations!');
 });
 
-//this endpoint triggers the app to simulate entering an unhealthy state by causing it to return 5XX errors.
+// This endpoint triggers the app to simulate entering an unhealthy state by causing it to return 5XX errors
 app.get('/break', function(req, res, next) {
-	broken = true;
-	res.status(200).send('The app is now broken!')
+  broken = true;
+  res.status(200).send('The app is now broken!');
 });
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Set the port and listen
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
